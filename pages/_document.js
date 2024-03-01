7a5581d0d9cc830b99ff3875bba824f6ec373505
Yotham <yothamsage@gmail.com>
2024-03-01 00:19:32 -0500

    google analytics

---
+++ pages/_document.js
@@ -0,0 +1,35 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          {/* Google Analytics Tag */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-762H0R0468"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-762H0R0468');
+              `,
+            }}
+          ></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
